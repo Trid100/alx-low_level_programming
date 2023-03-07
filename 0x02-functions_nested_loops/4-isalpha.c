5954bacef50e959f76ac7407055d8f8b048c11f9
Trid100 <danielphiri750@gmail.com>
2023-03-06 18:53:57 -0800

    4. isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,24 +1,18 @@
 #include "main.h"
 
+/**
+ * _isalpha - Checks for alphabetic character
+ * @c: The character to be checked
+ * Return: 1 for alphabetic character or 0 for anything else
+ */
 
-	/**
-	 * main - check the code.
-	 *
-	 * Return: Always 0.
-	 */
-	int main(void)
+int _isalpha(int c)
+
+{
+
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 	{
-	    int r;
-
-
-	    r = _isalpha('H');
-	    _putchar(r + '0');
-	    r = _isalpha('o');
-	    _putchar(r + '0');
-	    r = _isalpha(108);
-	    _putchar(r + '0');
-	    r = _isalpha(';');
-	    _putchar(r + '0');
-	    _putchar('\n');
-	    return (0);
+	return (1);
 	}
+	return (0);
+}
